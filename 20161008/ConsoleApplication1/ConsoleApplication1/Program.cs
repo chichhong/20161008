e4c641c7d52f690c25303312aebdfce711c53166
chichhong <chichhong@gmail.com>
2016-10-09 00:14:34 +0800

    add the input mumber from keyboard

--- 20161008/ConsoleApplication1/ConsoleApplication1/Program.cs
+++ 20161008/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,24 +1,50 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApplication1
-{
-    class Program
+ class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            int sum = 0, avg;
-            for (int a = 1; a <= 20 ; a++)
-            {
-                sum += a;
-            }
-            avg = sum / 20;
-            System.Console.Write(avg);
-            System.Console.Read();
+        int a = 0, sum = 0, avg;
+        //for (int a = 1; a <= 20; a++)
+        //{
+        //    sum += a;
+        //}
+
+
+        //sum += 1;
+        //sum += 2;
+        //sum += 3;
+        //sum += 4;
+        //sum += 5;
+        //sum += 6;
+        //sum += 7;
+        //sum += 8;
+        //sum += 9;
+        //sum += 10;
+        //sum += 11;
+        //sum += 12;
+        //sum += 13;
+        //sum += 14;
+        //sum += 15;
+        //sum += 16;
+        //sum += 17;
+        //sum += 18;
+        //sum += 19;
+        //sum += 20;
+
+
+        while (a <= 20)
+        {   sum += a;
+            a++;  
+        }
+
+        avg = sum / 20;
+            //Console.Write();
+            Console.WriteLine(avg);
+     
+            Console.Write(sum);
+            Console.Read();
             
         }
     }
-}
+
---
+++ 20161008/ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        public static void Main()
+        {
+            //ConsoleKeyInfo a =Console.ReadKey();
+            //int sum = 0;
+
+            //while (a.Key != ConsoleKey.Escape)
+            //{
+            //   int b  = Console.Read();
+
+            //    Console.WriteLine("you're typeing" + b);
+            //    sum += b;
+            //    Console.WriteLine("the total is sum");
+
+            //}
+
+
+            bool a = true;
+            int b;
+            int sum = 0,c=0;
+
+
+            while (a)
+            {
+                Console.WriteLine("輸入數字");
+                b = Int32.Parse(Console.ReadLine());
+               
+                if (b == 0)
+                {
+                    a = false;
+                }
+                else
+                {
+                    sum += b;
+                    c++;
+                }
+            }
+
+            //Console.WriteLine("輸入數字");
+            //b = Int32.Parse(Console.ReadLine());
+            //Console.WriteLine(b);
+
+            //Console.ReadKey();
+
+
+            Console.WriteLine(sum);
+            Console.WriteLine(c);
+            Console.WriteLine((float)sum/c);
+            Console.ReadKey();
+
+
+        }
+    }
+}  
