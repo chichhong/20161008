5dbde7ffc7fe503de716e37e2530796cf2f216cf
chichhong <chichhong@gmail.com>
2016-10-08 14:43:45 +0800

    C# add from 1 to 20

---
+++ 20161008/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0, avg;
+            for (int a = 1; a <= 20 ; a++)
+            {
+                sum += a;
+            }
+            avg = sum / 20;
+            System.Console.Write(avg);
+            System.Console.Read();
+            
+        }
+    }
+}
