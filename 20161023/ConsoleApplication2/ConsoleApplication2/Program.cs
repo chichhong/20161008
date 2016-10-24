61d8b54880304d9159d5997d16263d1242ccbdee
chichhong <chichhong@gmail.com>
2016-10-24 21:34:51 +0800

    sqrt
    數學的向量都是用一維陣列來實作
    例如:[3,4,5,6]表示一個四維的向量(含有四個元素3,4,5和6)
    
    一個向量A的長度寫成|A|
    若A=[a1,a2,a3,...,an], 則|A|=sqrt(a1*a1+a2*a2+a3*a3+...+an*an),
    其中sqrt式平方根
    例如若A=[1,2,1,2,1,2,1,2,1,2],
    則|A|=sqrt(1*1+2*2+1*1+2*2+1*1+2*2+1*1+2*2+1*1+2*2)
    =sqrt(25)
    =5
    把求長度寫成函式呼叫
    
    並請算出[1,5,15,25,35,45]的長度並輸出
    (你可以用函式庫提供的sqrt求平方根 好像是Math.sqrt 你可以查一下)

---
+++ 20161023/ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 1, 5, 15, 25, 35, 45 };
+            double sum = ArrayLengh(a);
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+        static double ArrayLengh(int[] a)
+        {
+            double sum = 0;
+            for (int i = 0; i < a.Length; i++) {
+                sum += a[i] * a[i];
+            }
+            sum = Math.Sqrt(sum);
+            return sum;
+        }
+    }
+}
