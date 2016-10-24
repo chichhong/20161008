ac9c92bc13f4d5bc8a1ab02a5676cca6bda077ee
chichhong <chichhong@gmail.com>
2016-10-24 13:06:51 +0800

    neiji
    陣列內積

--- 20161021/ConsoleApplication3/ConsoleApplication3/Program.cs
+++ 20161021/ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -11,12 +11,19 @@ namespace ConsoleApplication3
         static void Main(string[] args)
         {
             int[][] b = new int[][] { new int[] { 20,24,28},new int[] { 30,34,38},new int[] { 40,44,48},new int[] { 50,54,58} };
-            int[][] c = new int[][] { new int[b.GetLength(0)],new int[b.GetLength(1)]} ;
-
-
-            Add_1(b, c);
-        
+            int[][] c = new int[][] { new int[b.GetLength(0)], new int[b.GetLength(1)] };
             
+
+           Add_1(b, c);
+            foreach (int i in c) {
+                
+            }
+
+
+            c = Add_2(b);
+            foreach (int i in c) {
+
+            }
         }
 
 
@@ -30,8 +37,17 @@ namespace ConsoleApplication3
         }
 
 
-        static void Add_2(int[][] b) {
+        static int[][] Add_2(int[][] b) {
+            int[][] c = new int[][] { new int[b.GetLength(0)], new int[b.GetLength(1)] }; ;
+            for (int i = 0; i < b.GetLength(0); i++)
+            {
+                for (int j = 0; j < b.GetLength(1); j++)
+                {
+                    c[i][j] = b[i][j] + 1;
+                }
 
+            }
+            return c;
         }
     }
 }
---
+++ 20161023/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 1, 5, 15, 25, 35, 45 };
+            int[] b = { 1, 10, 20, 30, 40 ,1};
+            int c;
+            c = Naji(a, b);
+
+            Console.WriteLine(c);
+            Console.ReadKey();
+        }
+
+        static int Naji(int[] a,int[]b) {
+            int sum = 0;
+            for (int i = 0; i < a.Length; i++) {
+                sum += a[i] * b[i];
+            }
+            return sum;            
+        }
+    }
+}
