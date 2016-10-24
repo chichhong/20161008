4d08bdb604e8467a7bf6fc88adc08a94b2978b62
chichhong <chichhong@gmail.com>
2016-10-24 21:51:01 +0800

    yushain
    數學的向量都是用一維陣列來實作
    例如:[3,4,5,6]表示一個四維的向量(含有四個元素3,4,5和6)
    
    兩個向量A和B的相似度可以用餘弦值表示
    若A=[a1,a2,a3,...,an], B=[b1,b2,b3,...,bn]
    則A和B的餘弦值為
    A和B的餘弦值= A和B的內積/(|A||B|)
    其中|A|是A的長度 |B|是B的長度
    
    請寫出求任何兩個向量的餘弦值的程式
    並請算出[1,5,15,25,35,45]和[1,10,20,30,40,50]這兩個向量的餘弦值並輸出

---
+++ 20161023/ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 1, 5, 15, 25, 35, 45 };
+            int[] b = { 1, 10, 20, 30, 40, 50 };
+            Console.WriteLine(Yushain(a, b));
+            Console.ReadKey();
+        }
+
+        static double Yushain(int[] a, int[] b) {
+            double a_lengh = 0;
+            double b_lengh = 0;
+            double naiji = 0;
+            double sum = 0;
+            for (int i = 0; i < a.Length; i++) {
+                a_lengh += a[i] * a[i];
+            }
+            for (int i = 0; i < b.Length; i++) {
+                b_lengh += b[i] * b[i];
+            }
+            for (int i = 0; i < a.Length; i++) {
+                naiji += a[i] * b[i];
+            }
+            sum = naiji/Math.Sqrt(a_lengh)* Math.Sqrt(b_lengh);
+            return sum;
+            
+        }
+    }
+}
